fad7ead1503a2ff9193915dc20c6f79f2bbaed6c
Ozwin <ozwinneellobo12@gmail.com>
2024-03-06 17:31:42 -0500

    CSS fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
-import CssBaseline from '@mui/material/CssBaseline';
-import AppBar from '@mui/material/AppBar';
-import Container from '@mui/material/Container';
-import Toolbar from '@mui/material/Toolbar';
-import Paper from '@mui/material/Paper';
-import TextField from '@mui/material/TextField'
-import Button from '@mui/material/Button';
-import Grid from "@mui/material/Grid";
-import { Typography } from '@mui/material';
-import InputLabel from '@mui/material/InputLabel';
+import { AppBar, Toolbar } from '@mui/material';
+import { Container, Paper, Grid, CssBaseline } from '@mui/material';
+import { TextField, InputLabel, Autocomplete, Button, Typography } from '@mui/material';
 import { DataModel, Menu } from './interfaces';
 import { useForm, SubmitHandler } from "react-hook-form";
 import { handlePostMethod } from './services/api';
-import Autocomplete from '@mui/material/Autocomplete';
-import { Alert, Snackbar } from '@mui/material';
+import { Alert, Snackbar, AlertColor, useMediaQuery, SnackbarOrigin } from '@mui/material';
 import { useState } from 'react';
-import { AlertColor } from '@mui/material';
+
 function App() {
 
   const { register, formState: { errors }, handleSubmit, getValues } = useForm<DataModel>();
   const [isAlertOpen, setAlertOpen] = useState<boolean>(false);
   const [alertMessage, setAlertMessage] = useState<string>('');
   const [alertSeverity, setSeverity] = useState<AlertColor>("success");
+  const isMobileScreen = useMediaQuery('(max-width: 600px)');
+
+  const mobileAnchorOrigin: SnackbarOrigin = {
+    vertical: 'bottom',
+    horizontal: 'center',
+  };
+
+  const defaultAnchorOrigin: SnackbarOrigin = {
+    vertical: 'top',
+    horizontal: 'right',
+  };
+
+  const anchorOrigin = isMobileScreen ? mobileAnchorOrigin : defaultAnchorOrigin;
 
   const onSubmit: SubmitHandler<DataModel> = (data) => {
     handlePostMethod(data).then((status) => {
@@ -70,43 +75,43 @@ function App() {
           <img src={logo} className='image-layout' alt="logo" />
         </Toolbar>
       </AppBar>
+
+      {/* Common Alert publisher */}
+      <Snackbar
+        anchorOrigin={anchorOrigin}
+        open={isAlertOpen}
+        // autoHideDuration={5000}  // Auto hide after 5 seconds
+        onClose={handleCloseAlert}
+      >
+        <Alert
+          variant="filled"
+          severity={alertSeverity}
+          sx={{ position: 'absolute', top: 50, right: 25 }}
+        >
+          {alertMessage}
+        </Alert>
+      </Snackbar>
       <Container component="main" maxWidth="sm" sx={{ mb: 4 }}>
 
-        <Snackbar
-          anchorOrigin={{ vertical: 'top', horizontal: 'right', }}
-          open={isAlertOpen}
-          autoHideDuration={5000}  // Auto hide after 5 seconds
-          onClose={handleCloseAlert}
-        >
-          <Alert
-            variant="filled"
-            severity={alertSeverity}
-            sx={{ position: 'absolute', top: 50, right: 25 }}
-          >
-            {alertMessage}
-          </Alert>
-        </Snackbar>
-
-
-        <Grid container spacing={2}>
-          <Grid item xs={12}>
-            <Typography variant="h6" >
-              Create User Account
-            </Typography>
-
-            <Paper sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}>
-              <form onSubmit={handleSubmit(onSubmit)} >
+        {/* Sign up form starts here */}
+        <Grid container spacing={1} sx={{ mt: '20px' }}>
+          <form onSubmit={handleSubmit(onSubmit)} >
+            <Grid item xs={12} >
+              <Typography variant="h6" textAlign={'left'} pb={'16px'} >
+                Create User Account
+              </Typography>
+              <Paper sx={{ mb: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}>
                 <Grid container spacing={1}>
                   <Grid item xs={12}>
                     <InputLabel htmlFor="full_name" className='input-label'>Full Name</InputLabel>
                     <TextField
                       variant="outlined"
-                      required
+                      // required disabling default required feature to be more consistent with other validations
                       fullWidth
                       id="full_name"
-                      label="Full Name"
+                      label="Full Name*"
                       autoFocus
-                      {...register("full_name", { required: true, pattern: { value: /^[a-zA-Z\s]+$/i, message: "Sorry. Name can not include special symbols. Please use alphabets only." }, validate: (value) => { return value.trim().length !== 0 || "Name has only whitespace" } })}
+                      {...register("full_name", { required: "Thsi field is required", pattern: { value: /^[a-zA-Z\s]+$/i, message: "Sorry. Name can not include special symbols. Please use alphabets only." }, validate: (value) => { return value.trim().length !== 0 || "Name has only whitespace" } })}
                       error={errors.full_name ? true : false}
                       helperText={errors.full_name?.message}
                     />
@@ -115,13 +120,13 @@ function App() {
                     <InputLabel htmlFor="contact_number" className='input-label'>Contact Number</InputLabel>
                     <TextField
                       variant="outlined"
-                      required
+                      // required
                       fullWidth
                       id="contact_number"
                       label="Contact Number"
                       placeholder='4383048998'
                       autoFocus
-                      {...register("contact_number", { required: true, minLength: { value: 10, message: "Invalid number" }, maxLength: { value: 10, message: "Please enter your number without country code" }, pattern: { value: /^[0-9]+$/i, message: "Sorry. Please enter numbers only." } })}
+                      {...register("contact_number", { required: "Thsi field is required", minLength: { value: 10, message: "Invalid number" }, maxLength: { value: 10, message: "Please enter your number without country code" }, pattern: { value: /^[0-9]+$/i, message: "Sorry. Please enter numbers only." } })}
                       error={errors.contact_number ? true : false}
                       helperText={errors.contact_number?.message}
                     />
@@ -135,7 +140,7 @@ function App() {
                       disableClearable
                       id="day"
                       options={days}
-                      renderInput={(params) => <TextField required {...params} label="Day" {...register("date_of_birth.day", { required: "This field required" })} error={errors.date_of_birth?.day ? true : false}
+                      renderInput={(params) => <TextField  {...params} label="Day" {...register("date_of_birth.day", { required: "This field required" })} error={errors.date_of_birth?.day ? true : false}
                         helperText={errors.date_of_birth?.day?.message} />}
                     />
                   </Grid>
@@ -146,7 +151,7 @@ function App() {
                       id="month"
                       options={months}
                       // sx={{ width: 300 }}
-                      renderInput={(params) => <TextField required {...params} label="Month" {...register("date_of_birth.month", { required: "This field required" })} error={errors.date_of_birth?.month ? true : false}
+                      renderInput={(params) => <TextField  {...params} label="Month" {...register("date_of_birth.month", { required: "This field required" })} error={errors.date_of_birth?.month ? true : false}
                         helperText={errors.date_of_birth?.month?.message} />}
                     />
                   </Grid>
@@ -156,7 +161,7 @@ function App() {
                       disableClearable
                       id="year"
                       options={years}
-                      renderInput={(params) => <TextField required {...params} label="Year" {...register("date_of_birth.year", { required: "This field required" })} error={errors.date_of_birth?.year ? true : false}
+                      renderInput={(params) => <TextField  {...params} label="Year" {...register("date_of_birth.year", { required: "This field required" })} error={errors.date_of_birth?.year ? true : false}
                         helperText={errors.date_of_birth?.year?.message} />}
                     />
                   </Grid>
@@ -164,12 +169,12 @@ function App() {
                     <InputLabel htmlFor="email" className='input-label'>Email Address</InputLabel>
                     <TextField
                       variant="outlined"
-                      required
+                      // required
                       fullWidth
                       id="email"
                       label="Email Address"
                       autoFocus
-                      {...register("email", { required: true, pattern: { value: /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/i, message: "Sorry, This email address is not valid. Please try again" } })}
+                      {...register("email", { required: "Thsi field is required", pattern: { value: /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/i, message: "Sorry, This email address is not valid. Please try again" } })}
                       error={errors.email ? true : false}
                       helperText={errors.email?.message}
                     />
@@ -178,12 +183,12 @@ function App() {
                     <InputLabel htmlFor="password" className='input-label'>Password</InputLabel>
                     <TextField
                       variant="outlined"
-                      required
+                      // required
                       fullWidth
                       id="password"
                       label="Create Password"
                       autoFocus
-                      {...register("password", { required: true, minLength: 8, pattern: { value: /^[a-zA-Z0-9]+$/i, message: "Password can have alphabets and numbers only!" } })}
+                      {...register("password", { required: "Thsi field is required", minLength: { value: 8, message: "Please enter a password that is atleast 8 characters" }, pattern: { value: /^[a-zA-Z0-9]+$/i, message: "Password can have alphabets and numbers only!" } })}
                       error={errors.password ? true : false}
                       helperText={errors.password?.message}
                     />
@@ -192,13 +197,13 @@ function App() {
                     <InputLabel htmlFor="confirm_password" className='input-label'>Confirm Password</InputLabel>
                     <TextField
                       variant="outlined"
-                      required
+                      // required
                       fullWidth
                       id="confirm_password"
                       label="Confirm Password"
                       autoFocus
                       {...register("confirm_password", {
-                        required: true, minLength: 8, pattern: { value: /^[a-zA-Z0-9]+$/i, message: "Password can have alphabets and numbers only!" }, validate: (value) => {
+                        required: "Thsi field is required", minLength: { value: 8, message: "Please enter a password that is atleast 8 characters" }, pattern: { value: /^[a-zA-Z0-9]+$/i, message: "Password can have alphabets and numbers only!" }, validate: (value) => {
                           return value === getValues("password") || "Password don't match, please check and try again!";
                         }
                       })}
@@ -207,54 +212,34 @@ function App() {
 
                     />
                   </Grid>
-                  {/* <Grid item xs={12} sm={6}>
-                    <Button
-                      type="submit"
-                      variant="outlined"
-                      fullWidth
-                      color="primary"
-                      sx={{ borderColor: '#127C95' }}
-                    >
-                      Cancel
-                    </Button>
-                  </Grid>
-                  <Grid item xs={12} sm={6}>
-                    <Button
-                      type="submit"
-                      fullWidth
-                      variant="contained"
-                      color="primary"
-                    >
-                      Submit
-                    </Button>
-                  </Grid> */}
                 </Grid>
-              </form>
-            </Paper >
-            <Grid container spacing={1} justifyContent="center">
-              <Grid item xs={12} sm={4}>
-                <Button
-                  type="submit"
-                  variant="outlined"
-                  fullWidth
-                  color="primary"
-                  sx={{ borderColor: '#127C95' }}
-                >
-                  Cancel
-                </Button>
-              </Grid>
-              <Grid item xs={12} sm={4}>
-                <Button
-                  type="submit"
-                  fullWidth
-                  variant="contained"
-                  color="primary"
-                >
-                  Submit
-                </Button>
+
+              </Paper >
+              <Grid container spacing={1} justifyContent="center">
+                <Grid item xs={12} sm={4}>
+                  <Button
+                    type="submit"
+                    variant="outlined"
+                    fullWidth
+                    color="primary"
+                    sx={{ borderColor: '#127C95' }}
+                  >
+                    Cancel
+                  </Button>
+                </Grid>
+                <Grid item xs={12} sm={4}>
+                  <Button
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    color="primary"
+                  >
+                    Submit
+                  </Button>
+                </Grid>
               </Grid>
             </Grid>
-          </Grid>
+          </form>
         </Grid>
       </Container>
     </div >
